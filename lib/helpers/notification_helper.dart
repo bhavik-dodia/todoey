bf4ea033a8e76f467dcec9f38b177a9d3253e59f
bhavik-dodia <dodiabhavik.db@gmail.com>
2020-12-19 17:37:32 +0530

    organized imports

--- lib/helpers/notification_helper.dart
+++ lib/helpers/notification_helper.dart
@@ -1,8 +1,8 @@
+import 'package:Todoey/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
-import 'package:Todoey/main.dart';
+import 'package:timezone/timezone.dart' as tz;
 
 class NotificationHelper {
   Future<void> _showScheduledNotificationWithDefaultSound(
